4c2a86b5bad69d7dc633f7a2b1579504efe399a7
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2025-05-26 15:55:28 +0600

    removed performence visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,12 +12,12 @@ export default defineConfig({
 
   }),
 
-    visualizer({
-      filename: 'build/stats.html', // Updated to match new build directory
-      open: true, // automatically open the report in browser
-      gzipSize: true,
-      brotliSize: true,
-    })
+    // visualizer({
+    //   filename: 'build/stats.html', // Updated to match new build directory
+    //   open: true, // automatically open the report in browser
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // })
 
 
 ],
