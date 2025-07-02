009d0e0a07f4b1628f796151142861625807847a
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2025-07-02 15:42:29 +0600

    fix(toast): resolve issue where react-toastify toasts were not appearing
    Fixed a glitch preventing toast notifications from rendering correctly
    
    Ensured proper display behavior of react-toastify toasts

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from './components/Route/Router';
 import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+
  import { ConversationProvider } from './context/useConversationContext';
 import './index.css'
   function App() {
   return (
     <ConversationProvider>
       <RouterProvider router={router} />
-      <ToastContainer   />
     </ConversationProvider>
   );
     
